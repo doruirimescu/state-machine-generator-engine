4466bd66b50d4aa21e664c34dace5e9d248b6b80
Doru Irimescu <doru.irimescu@sensible4.fi>
2021-08-15 11:24:50 +0300

    Add testing env for procedural

---
+++ generated/procedural/test/test_procedural.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "header.h"
+
+TEST(StateMachine, ConstructStateMachineAndMove)
+{
+    ASSERT_EQ(true, true);
+}
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,20 @@
+function cppBuildAndTest()
+{
+    rm -rf build
+    mkdir build
+    cd build
+    cmake .. && make && make test
+    cd ..
+    rm -rf build
+}
+
+#Runs all python tests
 python3 -m pytest
+
+#Runs tests on generated oop C++ code
 cd generated/oop
-rm -rf build
-mkdir build
-cd build
-cmake .. && make && make test
+cppBuildAndTest
+
+#Runs tests on generated procedural C++ code
+cd ../procedural
+cppBuildAndTest
